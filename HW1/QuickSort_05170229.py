7cc63315daaaa3525abd403aea1a6ef9f817590a
Rita626 <55750276+Rita626@users.noreply.github.com>
2020-01-10 22:58:51 +0800

    Create QuickSort_05170229.py

---
+++ HW1/QuickSort_05170229.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def Quick_sort(self,list): 
+        self.list = list
+        a = [] #存放比key小的數值
+        b = [] #存放比key大的數值
+        c = [] #存放與key相同數值
+
+    if len(list) <= 1:
+        return list
+    #若list僅一值不需排序
+
+    else:
+        key = list[0] #另第一個數為key
+        for i in list:
+            if i < key:
+                a.append(i)
+            elif i > key:
+                b.append(i)
+            else:
+                c.append(i)
+
+    #將兩邊數列再重複排序
+    a = quick_sort(smaller)
+    b = quick_sort(bigger)
+    sort = a+c+b
+    return sort
