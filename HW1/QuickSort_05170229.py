4b3acf6eb3a366f0d263522955c6998901296ee4
Rita626 <55750276+Rita626@users.noreply.github.com>
2020-01-10 23:58:57 +0800

    Update QuickSort_05170229.py

--- HW1/QuickSort_05170229.py
+++ HW1/QuickSort_05170229.py
@@ -3,23 +3,22 @@ class Solution(object):
         a = [] #存放比key小的數值
         b = [] #存放比key大的數值
         c = [] #存放與key相同數值
-
-    if len(list) <= 1:
-        return list
-    #若list僅一值不需排序
-
-    else:
-        key = list[0] #另第一個數為key
-        for i in list:
-            if i < key:
-                a.append(i)
-            elif i > key:
-                b.append(i)
-            else:
-                c.append(i)
-
-    #將兩邊數列再重複排序
-    a = quick_sort(a)
-    b = quick_sort(b)
-    sort = a+c+b
-    return sort
+        
+        if len(list) <= 1:
+            return list
+        #若list僅一值不需排序
+        else:
+            key = list[0] #另第一個數為key
+            for i in list:
+                if i < key:
+                    a.append(i)
+                elif i > key:
+                    b.append(i)
+                else:
+                    c.append(i)
+        #將兩邊數列再重複排序
+        a = quick_sort(a)
+        b = quick_sort(b)
+        sort = a+c+b
+        
+        return sort
